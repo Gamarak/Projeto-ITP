9f367606cdedd6741336f2de0f85928312fe02d1
Arthur <arthusgamma@gmail.com>
2026-06-15 14:27:11 -0300

    Primeira Etapa: Criacao de uma paleta de cores

---
+++ paleta.cpp
@@ -0,0 +1,60 @@
+#include "paleta.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int HexInt(const string& hexString) {
+  int resultado = 0;
+  for (char c : hexString) {
+      int valorCaractere = 0;
+      c = toupper(c);
+        
+      if (c >= '0' && c <= '9') {
+          valorCaractere = c - '0';
+      } 
+      else if (c >= 'A' && c <= 'F') {
+           valorCaractere = c - 'A' + 10;
+      } 
+      else {
+          continue; 
+      }
+      resultado = (resultado * 16) + valorCaractere;
+  }
+  return resultado;
+}
+
+Paleta::Paleta(){}
+Paleta::Paleta(string nomeArquivo) {
+  ifstream arquivo(nomeArquivo);
+  if (!arquivo.is_open()){
+    cout << "Erro" << endl;
+    return;
+  }
+  string linha;
+
+  while (getline(arquivo, linha)) {
+    if (!linha.empty() && linha[0] == '#') {
+    int r = HexInt(linha.substr(1, 2));
+    int g = HexInt(linha.substr(3, 2));
+    int b = HexInt(linha.substr(5, 2));
+    Cor novaCor(r, g, b);
+    cores.push_back(novaCor);
+    }
+  }
+  arquivo.close();
+}
+
+void Paleta::adicionarCor(Cor c) {
+  cores.push_back(c);
+}
+
+int Paleta::obterTamanho() {
+  return cores.size();
+}
+
+Cor Paleta::obterCor(int indice) {
+  if(indice < 0 || indice >= cores.size()){
+    return Cor();
+  } 
+  return cores[indice];
+}
---
+++ paleta.h
@@ -0,0 +1,31 @@
+#ifndef paleta_h
+#define paleta_h
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Cor {
+  int r, g, b;
+  
+  Cor() : r(0), g(0), b(0){}
+
+  Cor(int vermelho, int verde, int azul){
+  r = max(0, min(255, vermelho));
+  g = max(0, min(255, verde));
+  b = max(0, min(255, azul));
+  }
+};
+
+class Paleta {
+  private:
+    vector<Cor> cores;
+
+  public:
+    Paleta();
+    Paleta(string nomeArquivo);
+    void adicionarCor(Cor c);
+    int obterTamanho();
+    Cor obterCor(int indice);
+};
+
+#endif
